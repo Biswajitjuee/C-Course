dbb26eb448fa1d6f1b150e42905fb0b0850870cd
Biswajit Ghosh <biswajitxi1998@gmail.com>
2021-09-29 20:18:47 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+
+   string  myarr[4]={"ji hello","Ji nameskar","ji prostitute","ji hullu"};
+   cout<<myarr[2]<<endl<<myarr[1];
+   return 0;
+}
---
+++ file1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int i=10;
+  i=i+15;
+  cout<<i<<endl;
+  cout<<"shut the fuck up";
+  return 0;
+
+}
